ed3e252aac936a1f8c5620d664858df0b6608c0a
Samuel K <skevetter@pm.me>
2026-04-21 00:40:42 -0500

    fix: prevent stdout corruption in kubernetes e2e tests (#24)
    ThrottledLogger in wait.go was using oldlog.GetInstance() which returns
    a StreamLogger that writes Info-level messages to stdout. In the agent
    process, stdout is the gRPC/tunnel protocol channel, so these log
    messages corrupted the binary protocol causing "did not receive a result
    back from agent" errors.
    
    Migrated ThrottledLogger to use pkg/log (which writes to stderr) instead
    of the old log library, matching the pattern established in PR #20.

--- pkg/driver/kubernetes/throttledlogger/throttled_logger.go
+++ pkg/driver/kubernetes/throttledlogger/throttled_logger.go
@@ -3,37 +3,35 @@ package throttledlogger
 import (
 	"time"
 
-	"github.com/devsy-org/log"
+	"github.com/devsy-org/devsy/pkg/log"
 )
 
 // ThrottledLogger is a logger that throttles the output,
 // i.e. it only logs a message if a certain amount of time has passed since the last log message.
 type ThrottledLogger struct {
-	logger log.Logger
-	timer  *Timer
+	timer *Timer
 }
 
-func NewThrottledLogger(logger log.Logger, throttlingInterval time.Duration) *ThrottledLogger {
+func NewThrottledLogger(throttlingInterval time.Duration) *ThrottledLogger {
 	return &ThrottledLogger{
-		logger: logger,
-		timer:  NewTimer(throttlingInterval),
+		timer: NewTimer(throttlingInterval),
 	}
 }
 
 func (t *ThrottledLogger) Infof(format string, args ...any) {
-	t.log(t.logger.Infof, format, args...)
+	t.logf(log.Infof, format, args...)
 }
 
 func (t *ThrottledLogger) Debugf(format string, args ...any) {
-	t.log(t.logger.Debugf, format, args...)
+	t.logf(log.Debugf, format, args...)
 }
 
-type LoggingFunc func(string, ...any)
+type loggingFunc func(string, ...any)
 
-func (t *ThrottledLogger) log(loggingFunc LoggingFunc, format string, args ...any) {
+func (t *ThrottledLogger) logf(fn loggingFunc, format string, args ...any) {
 	now := time.Now()
 	if t.timer.IntervalPassed(now) {
-		loggingFunc(format, args...)
+		fn(format, args...)
 		t.timer.Tick(now)
 	}
 }
