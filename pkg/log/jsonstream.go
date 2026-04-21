843765800a1a05a24e7bc758da255a9ba10562e6
Samuel K <skevetter@pm.me>
2026-04-21 05:01:22 -0500

    refactor(log): migrate PipeJSONStream and ReadJSONStream to package-level functions

--- pkg/log/jsonstream.go
+++ pkg/log/jsonstream.go
@@ -9,37 +9,34 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func PipeJSONStream(logger log.Logger) (io.WriteCloser, chan struct{}) {
+func PipeJSONStream() (io.WriteCloser, chan struct{}) {
 	done := make(chan struct{})
 	reader, writer := io.Pipe()
 	go func() {
-		ReadJSONStream(reader, logger)
+		ReadJSONStream(reader)
 		close(done)
 	}()
 
 	return writer, done
 }
 
-func ReadJSONStream(reader io.Reader, logger log.Logger) {
+var levelFuncs = map[logrus.Level]func(...any){
+	logrus.TraceLevel: Debug,
+	logrus.DebugLevel: Debug,
+	logrus.InfoLevel:  Info,
+	logrus.WarnLevel:  Warn,
+	logrus.ErrorLevel: Error,
+	logrus.PanicLevel: Error,
+	logrus.FatalLevel: Error,
+}
+
+func ReadJSONStream(reader io.Reader) {
 	scan := scanner.NewScanner(reader)
 	for scan.Scan() {
 		lineObject, err := Unmarshal(scan.Bytes())
 		if err == nil && lineObject.Message != "" {
-			switch lineObject.Level {
-			case logrus.TraceLevel:
-				logger.Debug(lineObject.Message)
-			case logrus.DebugLevel:
-				logger.Debug(lineObject.Message)
-			case logrus.InfoLevel:
-				logger.Info(lineObject.Message)
-			case logrus.WarnLevel:
-				logger.Warn(lineObject.Message)
-			case logrus.ErrorLevel:
-				logger.Error(lineObject.Message)
-			case logrus.PanicLevel:
-				logger.Error(lineObject.Message)
-			case logrus.FatalLevel:
-				logger.Error(lineObject.Message)
+			if fn, ok := levelFuncs[lineObject.Level]; ok {
+				fn(lineObject.Message)
 			}
 		}
 	}
