df33e8283080ba6ffea5f37a6ef8dcb74a25015c
Samuel K <skevetter@pm.me>
2026-04-22 13:48:19 -0500

    refactor: reduce complexity of InstallBinary (#59)
    * refactor: reduce complexity of InstallBinary
    
    * fix: address lint issues in InstallBinary refactor
    
    - Add #nosec G204 annotation for exec.Command with variable args
    - Convert if-else chain to switch statement per gocritic

--- pkg/git/install.go
+++ pkg/git/install.go
@@ -2,13 +2,13 @@ package git
 
 import (
 	"fmt"
+	"io"
 	"os/exec"
 
 	"github.com/devsy-org/devsy/pkg/command"
 	"github.com/devsy-org/devsy/pkg/log"
 )
 
-//nolint:cyclop
 func InstallBinary() error {
 	writer := log.Writer(log.LevelInfo)
 	errwriter := log.Writer(log.LevelError)
@@ -16,43 +16,18 @@ func InstallBinary() error {
 	defer func() { _ = errwriter.Close() }()
 
 	// try to install git via apt / apk
-	if !command.Exists("apt") && !command.Exists("apk") {
+	switch {
+	case command.Exists("apt"):
+		if err := installGitWithApt(writer, errwriter); err != nil {
+			return err
+		}
+	case command.Exists("apk"):
+		if err := installGitWithApk(writer, errwriter); err != nil {
+			return err
+		}
+	default:
 		// TODO: use golang git implementation
 		return fmt.Errorf("couldn't find a package manager to install git")
-	}
-
-	if command.Exists("apt") {
-		log.Infof("Git command is missing, try to install git with apt...")
-		cmd := exec.Command("apt", "update")
-		cmd.Stdout = writer
-		cmd.Stderr = errwriter
-		err := cmd.Run()
-		if err != nil {
-			return fmt.Errorf("run apt update: %w", err)
-		}
-		cmd = exec.Command("apt", "-y", "install", "git")
-		cmd.Stdout = writer
-		cmd.Stderr = errwriter
-		err = cmd.Run()
-		if err != nil {
-			return fmt.Errorf("run apt install git -y: %w", err)
-		}
-	} else if command.Exists("apk") {
-		log.Infof("Git command is missing, try to install git with apk...")
-		cmd := exec.Command("apk", "update")
-		cmd.Stdout = writer
-		cmd.Stderr = errwriter
-		err := cmd.Run()
-		if err != nil {
-			return fmt.Errorf("run apk update: %w", err)
-		}
-		cmd = exec.Command("apk", "add", "git")
-		cmd.Stdout = writer
-		cmd.Stderr = errwriter
-		err = cmd.Run()
-		if err != nil {
-			return fmt.Errorf("run apk add git: %w", err)
-		}
 	}
 
 	// is git available now?
@@ -63,4 +38,40 @@ func InstallBinary() error {
 	log.Infof("installed git")
 
 	return nil
+}
+
+func installGitWithApt(writer, errwriter io.Writer) error {
+	log.Infof("Git command is missing, try to install git with apt...")
+
+	if err := runCmd(writer, errwriter, "apt", "update"); err != nil {
+		return fmt.Errorf("run apt update: %w", err)
+	}
+
+	if err := runCmd(writer, errwriter, "apt", "-y", "install", "git"); err != nil {
+		return fmt.Errorf("run apt install git -y: %w", err)
+	}
+
+	return nil
+}
+
+func installGitWithApk(writer, errwriter io.Writer) error {
+	log.Infof("Git command is missing, try to install git with apk...")
+
+	if err := runCmd(writer, errwriter, "apk", "update"); err != nil {
+		return fmt.Errorf("run apk update: %w", err)
+	}
+
+	if err := runCmd(writer, errwriter, "apk", "add", "git"); err != nil {
+		return fmt.Errorf("run apk add git: %w", err)
+	}
+
+	return nil
+}
+
+func runCmd(stdout, stderr io.Writer, name string, args ...string) error {
+	cmd := exec.Command(name, args...) // #nosec G204 -- args are internally constructed
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	return cmd.Run()
 }
