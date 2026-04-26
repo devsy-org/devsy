0f903b63f02b6473a493bac7474fbd498165ee92
Samuel K <skevetter@pm.me>
2026-04-26 13:28:47 -0500

    fix(e2e): address goconst and golines lint findings

--- e2e/framework/timeouts.go
+++ e2e/framework/timeouts.go
@@ -5,29 +5,31 @@ import (
 	"time"
 )
 
+const osWindows = "windows"
+
 func TimeoutShort() time.Duration {
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS == osWindows {
 		return 10 * time.Minute
 	}
 	return 2 * time.Minute
 }
 
 func TimeoutModerate() time.Duration {
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS == osWindows {
 		return 25 * time.Minute
 	}
 	return 5 * time.Minute
 }
 
 func TimeoutLong() time.Duration {
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS == osWindows {
 		return 50 * time.Minute
 	}
 	return 10 * time.Minute
 }
 
 func TimeoutVeryLong() time.Duration {
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS == osWindows {
 		return 100 * time.Minute
 	}
 	return 20 * time.Minute
--- e2e/tests/ide/ide.go
+++ e2e/tests/ide/ide.go
@@ -17,39 +17,40 @@ var _ = ginkgo.Describe("devsy ide test suite", ginkgo.Label("ide"), ginkgo.Orde
 		framework.ExpectNoError(err)
 	})
 
-	ginkgo.It("start ides", ginkgo.SpecTimeout(framework.TimeoutShort()), func(ctx context.Context) {
-		f := framework.NewDefaultFramework(initialDir + "/bin")
-		tempDir, err := framework.CopyToTempDir("tests/ide/testdata")
-		framework.ExpectNoError(err)
-		ginkgo.DeferCleanup(framework.CleanupTempDir, initialDir, tempDir)
-
-		err = f.DevsyProviderAdd(ctx, "docker")
-		framework.ExpectNoError(err)
-		err = f.DevsyProviderUse(ctx, "docker")
-		framework.ExpectNoError(err)
-
-		ginkgo.DeferCleanup(func(cleanupCtx context.Context) {
-			err = f.DevsyWorkspaceDelete(cleanupCtx, tempDir)
+	ginkgo.It(
+		"start ides", ginkgo.SpecTimeout(framework.TimeoutShort()), func(ctx context.Context) {
+			f := framework.NewDefaultFramework(initialDir + "/bin")
+			tempDir, err := framework.CopyToTempDir("tests/ide/testdata")
 			framework.ExpectNoError(err)
+			ginkgo.DeferCleanup(framework.CleanupTempDir, initialDir, tempDir)
+
+			err = f.DevsyProviderAdd(ctx, "docker")
+			framework.ExpectNoError(err)
+			err = f.DevsyProviderUse(ctx, "docker")
+			framework.ExpectNoError(err)
+
+			ginkgo.DeferCleanup(func(cleanupCtx context.Context) {
+				err = f.DevsyWorkspaceDelete(cleanupCtx, tempDir)
+				framework.ExpectNoError(err)
+			})
+
+			err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=vscode")
+			framework.ExpectNoError(err)
+
+			err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=openvscode")
+			framework.ExpectNoError(err)
+
+			err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=jupyternotebook")
+			framework.ExpectNoError(err)
+
+			// TODO: Fix broken IDE
+			// err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=fleet")
+			// framework.ExpectNoError(err)
+
+			// check if ssh works
+			err = f.DevsySSHEchoTestString(ctx, tempDir)
+			framework.ExpectNoError(err)
+
+			// TODO: test jetbrains ides
 		})
-
-		err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=vscode")
-		framework.ExpectNoError(err)
-
-		err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=openvscode")
-		framework.ExpectNoError(err)
-
-		err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=jupyternotebook")
-		framework.ExpectNoError(err)
-
-		// TODO: Fix broken IDE
-		// err = f.DevsyUpWithIDE(ctx, tempDir, "--open-ide=false", "--ide=fleet")
-		// framework.ExpectNoError(err)
-
-		// check if ssh works
-		err = f.DevsySSHEchoTestString(ctx, tempDir)
-		framework.ExpectNoError(err)
-
-		// TODO: test jetbrains ides
-	})
 })
