31e33e9f2fc3a5b5b61f9e2144bfe16825070ccf
Samuel K <skevetter@pm.me>
2026-06-05 09:42:09 -0500

    chore: cleanup load test files

--- loadtest/emulateTraffic.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# SSH to the workspace and execute command
-for j in $(seq 1 "$NUM_COMMANDS_PER_WORKSPACE"); do
-    ./generateLoad.sh "$1"
-    sleep 1
-done
--- loadtest/generateLoad.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-devsy ssh "loadtest$1" --command="tr -dc A-Za-z0-9 </dev/urandom | head -c 100000000; echo" >/dev/null
