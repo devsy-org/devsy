935372a457ae6a32975533f92b4c67456c00f25c
Samuel K <skevetter@pm.me>
2026-06-05 15:29:07 -0500

    chore: cleanup load test files (#492)

--- loadtest/init_monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#kubectl -n devsy-pro set env deployment/devsy DEVSYDEBUG=true
-
-kubectl -n devsy-pro port-forward "$(kubectl -n devsy-pro get pods -l app=devsy -o jsonpath="{.items[0].metadata.name}")" 8080:8080
