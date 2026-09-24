b05c96a51a61197d11d16dea0b76ea0504d5f23a
Samuel K <skevetter@pm.me>
2026-09-24 16:17:21 -0600

    style(e2e): format feature installer

--- e2e/tests/build/testdata/docker-compose-features-context/.devcontainer/features/context-build/install.sh
+++ e2e/tests/build/testdata/docker-compose-features-context/.devcontainer/features/context-build/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-printf '%s\n' 'context-build-feature-installed' > /usr/local/bin/context-build-feature
+printf '%s\n' 'context-build-feature-installed' >/usr/local/bin/context-build-feature
 chmod +x /usr/local/bin/context-build-feature
